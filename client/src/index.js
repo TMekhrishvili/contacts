3acb95f607f735ee817ae48823cb75e87bb89590
Tamar Mekhrishvili <tamar.mekhrishvili@medsoft.ge>
2021-03-25 01:19:19 +0400

    url changing

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://localhost:44374/';
+axios.defaults.baseURL = 'https://young-tor-17286.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
