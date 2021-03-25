54e80cf3031e24204260065a382ca99fa938fa9d
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-03-26 00:17:38 +0400

    url

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3001/'//'https://young-tor-17286.herokuapp.com/'
+axios.defaults.baseURL = 'https://young-tor-17286.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 
