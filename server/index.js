858453c28911e0288149eea5ed5d7dfc022a5f4c
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-03-25 14:02:48 +0400

    server structure

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const dotenv = require('dotenv')
 
 app.use(cors())
 app.use(express.json())
+
+dotenv.config({ path: './config.env' })
 
 app.get('/', (req, res) => {
     res.send({ hello: "world" })
