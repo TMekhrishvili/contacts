3046776ecf91584b3aaa1376d0d4fc3e54ba5761
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2021-03-25 15:05:09 +0400

    test

--- server/index.js
+++ server/index.js
@@ -2,12 +2,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
 
 app.use(cors())
 app.use(express.json())
-
-dotenv.config({ path: './config.env' })
-
 app.use('/', require('./server/routes'))
 
 const PORT = process.env.PORT || 3001
